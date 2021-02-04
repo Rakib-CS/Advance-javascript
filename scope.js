c20c297f347f0782779c3697d954f51b0e8530ed
Rakib-CS <official.arrakib@gmail.com>
2021-02-05 02:46:01 +0600

    advance-javascript

---
+++ scope.js
@@ -0,0 +1,22 @@
+let bonus=20;
+
+function sum(first,second){
+    let result= first+second;
+    //console.log(result);
+    console.log(bonus);
+    if(result>9){
+        let mood ="happy";
+        mood ="fishy";
+        mood ="cranky";
+        mood ="funky";
+        //console.log(mood);
+    }
+   
+    var day ="friday"
+    console.log(day);
+    return result;
+}
+
+const output = sum(3,7);
+console.log(bonus);
+console.log(output);
