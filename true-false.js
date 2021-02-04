0e632dc08f9d0e6c3ac86656b2ddbfa80be8452b
Rakib-CS <official.arrakib@gmail.com>
2021-02-04 14:46:50 +0600

    true-false

---
+++ true-false.js
@@ -0,0 +1,39 @@
+//const age = 4;
+
+
+//if (age > 0){
+    //let Name=null;
+
+    //let Name=NaN;
+    //console.log(Name);
+//const Name2 = "salmankhan";
+//if (Name2.length > 0) {
+
+// if(Name){
+//     console.log("Condition is true");
+// }
+// else {
+//     console.log("Condition is False");
+// }
+
+let Name='false';
+if(Name||Name==0){
+    console.log("conditiin is true");
+}
+else{
+    console.log("condition is false")
+}
+
+
+
+//falsy
+//false
+// 0
+//""
+//undefined
+//null
+//nun
+
+
+// Truthy
+// '0' , ' ', []
